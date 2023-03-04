8cb85ac9747fb3219e6238071acd469562a43f9d
z02 <miigo@duck.com>
2023-03-04 12:49:20 +0700

    StringBuilder

---
+++ src/franxx/code/classes/StringBuilderApp.java
@@ -0,0 +1,27 @@
+package franxx.code.classes;
+
+public class StringBuilderApp {
+    public static void main(String[] args) {
+        // Tidak di sarankan
+        String name = "Hilmi";
+        name = name + " " + "Akbar";
+        name = name + " " + "Muharrom";
+        System.out.println(name+": boros memory");
+        // walaupun outputnya Hilmi Akbar Muharrom, tidak di sarankan
+        // Hilmi
+        // Akbar
+        // Muharrom
+
+        // disaran kan
+        StringBuilder builder = new StringBuilder();
+        builder.append("Hilmi");
+        builder.append(" ");
+        builder.append("Akbar");
+        builder.append(" ");
+        builder.append("Muharrom");
+
+        String names = builder.toString();
+        System.out.println(names+": irit memory");
+
+    }
+}

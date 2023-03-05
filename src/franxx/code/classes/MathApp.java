67f19ff688954258ce76c32a683f34c6586e9f1d
z02 <miigo@duck.com>
2023-03-05 13:03:23 +0700

    Math Class Static Method

---
+++ src/franxx/code/classes/MathApp.java
@@ -0,0 +1,12 @@
+package franxx.code.classes;
+
+public class MathApp {
+    public static void main(String[] args) {
+        int min = Math.min(1000, 12300);
+        int max = Math.max(900009, 8991821);
+
+        System.out.println("Minimum value: " + min);
+        System.out.println("Maximum value: " + max);
+        System.out.println(Math.PI);
+    }
+}

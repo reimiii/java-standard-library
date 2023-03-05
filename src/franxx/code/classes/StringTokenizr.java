ed7b2c7aeef76a126e145c6d79c1d27c4b30ad4c
z02 <miigo@duck.com>
2023-03-05 12:33:10 +0700

    StringTokenizer

---
+++ src/franxx/code/classes/StringTokenizr.java
@@ -0,0 +1,15 @@
+package franxx.code.classes;
+
+import java.util.StringTokenizer;
+
+public class StringTokenizr {
+    public static void main(String[] args) {
+        String name = "Hilmi-Akbar-#Muharrom";
+        StringTokenizer tokenizer = new StringTokenizer(name, "-");
+
+        while (tokenizer.hasMoreTokens()){
+            String tk = tokenizer.nextToken();
+            System.out.println(tk);
+        }
+    }
+}

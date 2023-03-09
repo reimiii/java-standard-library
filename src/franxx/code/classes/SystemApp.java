a27a3c38e35a67ac202b5e58cdb07ed2ecc40cde
z02 <miigo@duck.com>
2023-03-09 19:12:06 +0700

    System class

---
+++ src/franxx/code/classes/SystemApp.java
@@ -0,0 +1,10 @@
+package franxx.code.classes;
+
+public class SystemApp {
+    public static void main(String[] args) {
+        System.out.println(System.currentTimeMillis());
+        System.out.println(System.nanoTime());
+
+        System.out.println(System.getenv("HOME"));
+    }
+}
